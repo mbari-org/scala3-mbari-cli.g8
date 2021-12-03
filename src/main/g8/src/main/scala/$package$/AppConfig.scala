b641085646a7275e512455afc10d88b852483286
Brian Schlining <bschlining@gmail.com>
2021-12-03 08:55:37 -0800

    Updated AppConfig

--- src/main/g8/src/main/scala/$package$/AppConfig.scala
+++ src/main/g8/src/main/scala/$package$/AppConfig.scala
@@ -2,6 +2,8 @@ package $package$
 
 object AppConfig {
 
+  val Config = ConfigFactory.load()
+
   val Name = "$name$"
 
   val Version: String = Try(getClass.getPackage.getImplementationVersion).getOrElse("0.0.0-SNAPSHOT")
