aef8567142230be9e151d32a0f8b6e766a152ea7
Brian Schlining <bschlining@gmail.com>
2021-12-14 13:51:27 -0800

    Added implicit  method to circe

--- src/main/g8/src/main/scala/$package$/etc/circe/CirceCodecs.scala
+++ src/main/g8/src/main/scala/$package$/etc/circe/CirceCodecs.scala
@@ -21,3 +21,20 @@ object CirceCodecs:
   given urlEncoder: Encoder[URL] = Encoder
     .encodeString
     .contramap(_.toString)
+
+  private val printer = Printer.noSpaces.copy(dropNullValues = true)
+
+  /**
+   * Convert a circe Json object to a JSON string
+   * @param value
+   *   Any value with an implicit circe coder in scope
+   */
+  extension (json: Json) def stringify: String = printer.print(json)
+
+  /**
+   * Convert an object to a JSON string
+   * @param value
+   *   Any value with an implicit circe coder in scope
+   */
+  extension [T: Encoder](value: T) def stringify: String = Encoder[T].apply(value).stringify
+
