8f73618498a11060fa563ce145690e8be7fab493
Brian Schlining <bschlining@gmail.com>
2022-08-15 10:49:52 -0700

    Updated dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -8,17 +8,17 @@ object Dependencies {
   lazy val circeParser     = "io.circe" %% "circe-parser"  % circeVersion
 
   lazy val jansi    = "org.fusesource.jansi"         % "jansi"           % "2.4.0"
-  lazy val logback  = "ch.qos.logback"               % "logback-classic" % "1.3.0-alpha16"
+  lazy val logback  = "ch.qos.logback"               % "logback-classic" % "1.3.0-beta0"
   lazy val methanol = "com.github.mizosoft.methanol" % "methanol"        % "1.7.0"
   lazy val munit    = "org.scalameta"               %% "munit"           % "1.0.0-M6"
   lazy val picocli  = "info.picocli"                 % "picocli"         % "4.6.3"
 
-  lazy val slf4jVersion = "2.0.0-alpha7"
+  lazy val slf4jVersion = "2.0.0-beta1"
   lazy val slf4jApi     = "org.slf4j"      % "slf4j-api"                    % slf4jVersion
   lazy val slf4jJul     = "org.slf4j"      % "jul-to-slf4j"                 % slf4jVersion
   lazy val slf4jJdk     = "org.slf4j"      % "slf4j-jdk-platform-logging"   % slf4jVersion
 
-  lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.4.1"
+  lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.4.2"
   lazy val zio            = "dev.zio"       %% "zio"             % "2.0.0"
   
 }
